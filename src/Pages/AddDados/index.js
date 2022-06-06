c0c2e198a91b9c15abd963a56dbb0a5c79537112
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2022-06-06 10:39:30 -0300

    Gravação e Leitura de registro específico

--- src/Pages/AddDados/index.js
+++ src/Pages/AddDados/index.js
@@ -1,22 +1,36 @@
 // BD 'estudo' no Firebase
 import React, { useState } from 'react';
 import firebase from '../../services/firebaseConnection';
-function AddDados() {
 
+function AddDados() {
     const [freq, setFreq] = useState(0);
     const [msg, setMsg] = useState('');
 
     async function cadastrar() {
         await firebase.firestore().collection('fichas')
-        .doc('123')    
-        .set({
+            .doc('123')
+            .set({
                 frequencia: freq,
                 mensagems: msg
             })
-            .then(()=>{
+            .then(() => {
                 alert('Dados cadastrados com sucesso!');
                 setFreq(0);
                 setMsg('');
+            })
+            .catch((error) => {
+                alert('Erro ao cadastar:', error);
+            })
+    }
+
+    async function ler() {
+        await firebase.firestore().collection('fichas')
+            .doc('123')
+            .get()
+            .then((snapshot) => {
+                setFreq(snapshot.data().frequencia);
+                setMsg(snapshot.data().mensagems);
+                console.log('Dados lidos com sucesso! ', freq, " e ", msg);
             })
             .catch((error) => {
                 alert('Erro ao cadastar:', error);
@@ -31,6 +45,7 @@ function AddDados() {
             <label>Mensagem:</label>
             <textarea cols="60" rows="10" value={msg} onChange={(e) => { setMsg(e.target.value) }} /><br />
             <button onClick={cadastrar}>Cadastrar</button>
+            <button onClick={ler}>Ler id: 123</button>
         </div>
     );
 }
