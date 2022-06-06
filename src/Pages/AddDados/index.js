6b8513c8a4a63d8acb3feda20f44ac5b481f368e
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2022-06-06 20:03:58 -0300

    Firebase: all registers

--- src/Pages/AddDados/index.js
+++ src/Pages/AddDados/index.js
@@ -5,11 +5,28 @@ import firebase from '../../services/firebaseConnection';
 function AddDados() {
     const [freq, setFreq] = useState(0);
     const [msg, setMsg] = useState('');
+    const [lista, setLista] = useState([]);
 
     async function cadastrar() {
         await firebase.firestore().collection('fichas')
             .doc('123')
             .set({
+                frequencia: freq,
+                mensagems: msg
+            })
+            .then(() => {
+                alert('Dados cadastrados com sucesso!');
+                setFreq(0);
+                setMsg('');
+            })
+            .catch((error) => {
+                alert('Erro ao cadastar:', error);
+            })
+    }
+
+    async function cadastrarIdAutomatico() {
+        await firebase.firestore().collection('fichas')
+            .add({
                 frequencia: freq,
                 mensagems: msg
             })
@@ -37,6 +54,31 @@ function AddDados() {
             })
     }
 
+
+    useState (()=>{
+        async function buscarTodos() {
+            await firebase.firestore().collection('fichas')
+                .get()
+                .then((snapshot) => {
+                    let itens = [];
+
+                    snapshot.forEach((doc)=>{
+                        itens.push({
+                            id: doc.data().id,
+                            freq: doc.data().frequencia,
+                            mensagem: doc.data().mensagems
+                        });
+                    })
+                    setLista(itens);
+                })
+                .catch((error) => {
+                    alert('Erro ao listar:', error);
+                })
+        }
+        buscarTodos();
+
+    },[]);
+
     return (
         <div>
             <h2>Adiciona dos dados ao BD:</h2>
@@ -46,6 +88,20 @@ function AddDados() {
             <textarea cols="60" rows="10" value={msg} onChange={(e) => { setMsg(e.target.value) }} /><br />
             <button onClick={cadastrar}>Cadastrar</button>
             <button onClick={ler}>Ler id: 123</button>
+            <button onClick={cadastrarIdAutomatico}>Cadastro com Id automatico</button>
+            <br/><br/>
+            <hr/>
+            <h2>Registros armazenados:</h2>
+            {lista.map((item)=>{
+                return(
+                    <>
+                        <span key={item.id}>({item.freq})</span>
+                        <strong> {item.mensagem}</strong>
+                        <br/>
+                        <br/>
+                    </>
+                )
+            })}
         </div>
     );
 }
