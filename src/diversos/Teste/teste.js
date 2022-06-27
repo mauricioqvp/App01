bbe8132f423434df556ec06b5f9c3a8dfd8f6e5d
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2022-06-27 20:56:20 -0300

    API Navigator Clipboard working ok

--- src/diversos/Teste/teste.js
+++ src/diversos/Teste/teste.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
+// https://www.delftstack.com/pt/howto/javascript/javascript-copy-to-clipboard/#copiar-texto-para-a-%25C3%25A1rea-de-transfer%25C3%25AAncia-usando-clipboard-api-em-javascript
 
 function Teste() {
-    
+
     const [conteudo, setConteudo] = useState('');
 
     useEffect(() => {
@@ -10,11 +11,29 @@ function Teste() {
     function copyToClipBoard(e) {
         e.preventDefault();
 
-        var content = document.getElementById('textArea');
-        content.select();
-        document.execCommand('copy');
-        setConteudo(content.textContent);
-        alert(content.textContent);
+        //var content = document.getElementById('textArea');
+        //content.select();
+        //document.execCommand('copy');
+        //setConteudo(content.textContent);
+        //alert(content.textContent);
+
+        if (!navigator.clipboard) {
+            console.log("seu naveador não suporta API Clipboard");
+            return
+        }
+
+        var content = document.getElementById('textArea').innerHTML;
+
+        var texto = 'mauricio';
+
+        // solução: copia um conteúdo para a Área de Transferência
+        navigator.clipboard.writeText(texto)
+            .then(() => {
+                console.log("Text copied to clipboard...")
+            })
+            .catch(err => {
+                console.log('Something went wrong', err);
+            })
     }
 
     return (
@@ -26,7 +45,7 @@ function Teste() {
                 </textarea>
                 <textarea id="vazio"
                     value={conteudo}
-                    >
+                >
                 </textarea>
                 <button type="submit">Copy</button>
             </form>
